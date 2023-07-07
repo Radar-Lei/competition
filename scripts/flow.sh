f27e6b70ca89b1cc299b811cefc72682e16a9001
Radar-Lei <greatradar@gmail.com>
2023-07-07 23:40:12 +0800

    upda pa

--- scripts/flow.sh
+++ scripts/flow.sh
@@ -2,40 +2,13 @@ python -u main.py \
     --is_training 0 \
     --root_path ./dataset/competition/train-5min/ \
     --data_path flow.csv \
-    --d_model 32 \
-    --d_ff 32 \
-    --gpu 1 \
-    --num_kernels 3 \
-    --top_k 3 \
-    --lradj type3 \
-    --batch_size 32 \
-    --patience 10 \
-    --freq h \
-
-python -u main.py \
-    --is_training 0 \
-    --root_path ./dataset/competition/train-5min/ \
-    --data_path flow.csv \
-    --d_model 64 \
-    --d_ff 64 \
+    --d_model 256 \
+    --d_ff 256 \
     --gpu 1 \
     --num_kernels 6 \
     --top_k 5 \
     --lradj type3 \
-    --batch_size 32 \
-    --patience 10 \
-    --freq h \
-
-python -u main.py \
-    --is_training 0 \
-    --root_path ./dataset/competition/train-5min/ \
-    --data_path flow.csv \
-    --d_model 64 \
-    --d_ff 64 \
-    --gpu 1 \
-    --num_kernels 6 \
-    --top_k 3 \
-    --lradj type3 \
-    --batch_size 32 \
     --patience 10 \
     --freq t \
+    --learning_rate 0.001\
+    --batch_size 16\
