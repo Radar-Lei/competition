c487f58a2b49942142d2c35eb70080c9c39b162c
Radar-Lei <greatradar@gmail.com>
2023-07-07 23:01:27 +0800

    updatea

--- scripts/flow.sh
+++ scripts/flow.sh
@@ -2,8 +2,8 @@ python -u main.py \
     --is_training 0 \
     --root_path ./dataset/competition/train-5min/ \
     --data_path flow.csv \
-    --d_model 64 \
-    --d_ff 64 \
+    --d_model 32 \
+    --d_ff 32 \
     --gpu 0 \
     --num_kernels 6 \
     --top_k 5 \
@@ -16,8 +16,8 @@ python -u main.py \
     --is_training 0 \
     --root_path ./dataset/competition/train-5min/ \
     --data_path flow.csv \
-    --d_model 32 \
-    --d_ff 32 \
+    --d_model 64 \
+    --d_ff 64 \
     --gpu 0 \
     --num_kernels 6 \
     --top_k 5 \
