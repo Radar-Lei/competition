7290f413cc95b244097a6e1920165c13546b3a2c
Radar-Lei <greatradar@gmail.com>
2023-07-25 08:53:41 +0800

    ok

--- exp_imputation.py
+++ exp_imputation.py
@@ -157,13 +157,14 @@ class Exp_Imputation(Exp_Basic):
                                 pred[:, j] * (1 - mask[0, :, j].detach().cpu().numpy())
                     visual(true[:, j], filled, os.path.join(folder_path, str(epoch) + '_' + str(j) + '.png'))
 
-            print("Epoch: {} cost time: {}".format(epoch + 1, time.time() - epoch_time))
+            train_end_time = time.time()
+            print("Epoch: {} training time cost: {}".format(epoch + 1, train_end_time - epoch_time))
             train_loss = np.average(train_loss)
             vali_loss = self.vali(vali_data, vali_loader, criterion)
             test_loss = self.vali(test_data, test_loader, criterion)
 
-            print("Epoch: {0}, Steps: {1} | Train Loss: {2:.7f} Vali Loss: {3:.7f} Test Loss: {4:.7f}".format(
-                epoch + 1, train_steps, train_loss, vali_loss, test_loss))
+            print("Epoch: {0}, eval time cost: {1:.2f}, Steps: {2} | Train Loss: {3:.7f} Vali Loss: {4:.7f} Test Loss: {5:.7f}".format(
+                epoch + 1, time.time()-train_end_time, train_steps, train_loss, vali_loss, test_loss))
             early_stopping(vali_loss, self.model, path)
             if early_stopping.early_stop:
                 print("Early stopping")
