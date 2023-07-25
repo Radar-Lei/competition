8070a7c3bc2c39177ed5f340421009477b5e21ad
Radar-Lei <greatradar@gmail.com>
2023-07-25 18:26:58 +0800

    not convergence

--- main.py
+++ main.py
@@ -50,8 +50,8 @@ parser.add_argument('--factor', type=int, default=3, help='attn factor') # what 
 parser.add_argument('--enc_in', type=int, default=40, help='encoder input size') # dim of feature/ num of nodes
 parser.add_argument('--dec_in', type=int, default=40, help='decoder input size')
 parser.add_argument('--c_out', type=int, default=40, help='output size')
-parser.add_argument('--d_model', type=int, default=32, help='dimension of model') # 512
-parser.add_argument('--d_ff', type=int, default=64, help='dimension of fcn') # FC network, 2048
+parser.add_argument('--d_model', type=int, default=128, help='dimension of model') # 512
+parser.add_argument('--d_ff', type=int, default=256, help='dimension of fcn') # FC network, 2048
 parser.add_argument('--top_k', type=int, default=5, help='for TimesBlock') # 5
 parser.add_argument('--num_kernels', type=int, default=6, help='for Inception') # 6
 parser.add_argument('--embed', type=str, default='timeF',
@@ -73,7 +73,7 @@ parser.add_argument('--des', type=str, default='Exp', help='exp description')
 parser.add_argument('--itr', type=int, default=1, help='experiments times') # num of experiments
 parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')
 parser.add_argument('--patience', type=int, default=30, help='early stopping patience')
-parser.add_argument('--learning_rate', type=float, default=0.001, help='optimizer learning rate')
+parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate')
 parser.add_argument('--use_amp', action='store_true', help='use automatic mixed precision training', default=False)
 parser.add_argument('--train_epochs', type=int, default=500, help='train epochs')
 
